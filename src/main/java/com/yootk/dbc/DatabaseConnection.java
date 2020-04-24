d033b43699097a8ca30cc516346f17ad0058ffa8
LeeHappy <happy@git.com>
2020-04-24 21:45:12 +0800

    创建了一个连接数据库的程序类
    Signed-off-by: LeeHappy <happy@git.com>

--- src/main/java/com/yootk/dbc/DatabaseConnection.java
+++ src/main/java/com/yootk/dbc/DatabaseConnection.java
@@ -1,4 +1,7 @@
 package com.yootk.dbc;
 
 public class DatabaseConnection {
+
+    private static final String DB_DRIVER= "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL="jdbc:mysql://localhost:3306/test" ;
 }
